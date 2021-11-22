ae889a6d55a28aca3a2217518ddabfa76e1e4a01
jazielloureiro <jazielloureiro17@gmail.com>
2021-11-22 13:35:29 -0300

    Reverse lists

--- roman.hs
+++ roman.hs
@@ -1,8 +1,8 @@
 import Data.List
 import Data.Maybe
 
-roman = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
-decimal = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
 getIndex x list = fromJust (elemIndex x list)
 
@@ -38,6 +38,6 @@ divDec x dec =
 
 toRoman x =
     if 1 <= x && x <= 3999 then
-        divDec x (reverse decimal)
+        divDec x decimal
     else
         ""
