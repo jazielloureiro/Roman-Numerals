0ccab36c79bce98c2e85cb23729339aa94287e84
jazielloureiro <jazielloureiro17@gmail.com>
2022-02-13 23:23:02 -0300

    Add isDec function

--- src/romanc.hs
+++ src/romanc.hs
@@ -1,6 +1,9 @@
 import Roman
 import System.Environment
 
+isDec "" = True
+isDec (x:xs) = elem x ['0'..'9'] && isDec xs
+
 help progName = do
     putStrLn $ "Usage:"
     putStrLn $ "  " ++ progName ++ " <input>"
