9961ef1c116c839e78b46c22cab274296f026fba
jazielloureiro <jazielloureiro17@gmail.com>
2022-02-13 23:20:18 -0300

    Add help function

--- src/romanc.hs
+++ src/romanc.hs
@@ -1,2 +1,7 @@
 import Roman
 import System.Environment
+
+help progName = do
+    putStrLn $ "Usage:"
+    putStrLn $ "  " ++ progName ++ " <input>"
+    putStrLn $ "  " ++ progName ++ " [-h | --help]"
